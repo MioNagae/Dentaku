94656c092681e9188c231c256d32ea5fa0692481
EC2 Default User <ec2-user@ip-172-31-52-167.ec2.internal>
2023-10-24 14:28:42 +0000

    演算子の連続入力抑制

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 /*------------基本的な演算子（出力結果）--------------*/
-
 let calc = document.getElementById("result");
-let operate = document.getElementsByClassName("button cal");
-
 function clickButton(display) {
   let display_value = display.innerHTML;
   
@@ -10,6 +7,79 @@ function clickButton(display) {
     calc.innerHTML = "0";
   }else if(display_value == "="){//「＝」が押されたとき
     calc.innerHTML = eval(calc.innerHTML)
+    
+//力技で演算子の連続抑制
+//「＋」押した時
+  }else if(display_value == "+"){
+    console.log("+押したよ");
+    let f = calc.innerHTML
+    if(f.slice(-1) === "+"){
+      console.log(f);
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else if(f.slice(-1) === "-"){
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else if(f.slice(-1) === "/"){
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else if(f.slice(-1) === "*"){
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else{
+      calc.innerHTML += display_value; 
+    }
+  //「ー」押した時
+  }else if(display_value == "-"){
+    console.log("-押したよ");
+    let f = calc.innerHTML
+    if(f.slice(-1) === "+"){
+      console.log(f);
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else if(f.slice(-1) === "-"){
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else if(f.slice(-1) === "/"){
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else if(f.slice(-1) === "*"){
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else{
+      calc.innerHTML += display_value; 
+    }
+  //「/」押した時
+  }else if(display_value == "/"){
+    console.log("+押したよ");
+    let f = calc.innerHTML
+    if(f.slice(-1) === "+"){
+      console.log(f);
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else if(f.slice(-1) === "-"){
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else if(f.slice(-1) === "/"){
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else if(f.slice(-1) === "*"){
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else{
+      calc.innerHTML += display_value; 
+    }
+  ////「*」押した時
+  }else if(display_value == "*"){
+    console.log("-押したよ");
+    let f = calc.innerHTML
+    if(f.slice(-1) === "+"){
+      console.log(f);
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else if(f.slice(-1) === "-"){
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else if(f.slice(-1) === "/"){
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else if(f.slice(-1) === "*"){
+      calc.innerHTML = f.slice(0,-1) + display_value ;
+    }else{
+      calc.innerHTML += display_value; 
+    }
+  
+//先頭に０がつかないように制御する
+  
+  
+  
+
+    
   }else{//上記以外が押されたとき
     if(calc.innerHTML == "0"){
       calc.innerHTML = display_value;
@@ -19,17 +89,6 @@ function clickButton(display) {
     }
   }
 }
-
-function operating(display) {//演算子を押した時のfunction
-  const preValue = document.display.value.slice(-1);//前回入力したボタン、を定義
-  let display_value = display.innerHTML;
-  if(display_value == "+"　&& display.preValue == "+"){//「＋」が押された時、かつ前回の入力が「＋」なら
-    return;
-  }else{
-    calc.innerHTML += display_value;
-  }
-}
-
 
 
 
@@ -43,9 +102,3 @@ function operating(display) {//演算子を押した時のfunction
   //     e.target.disabled = false;
   //   }
   // })
-
-
-/*----------一番右に演算子が来たとき、次に演算子を入力したら上書きされるようにする
-式の一番右が演算子（+-/*）の場合→入力された演算子を上書き（表示されている式の一番右の演算子をカットして入力された演算子を追加）
-表示されている式の一番右が演算子でない場合→表示されている式に入力された演算子を追加---------*/
-
